e0c48f53bc64078667134775e4c21364e558884a
VenkonProgram <luisev96@hotmail.com>
2016-04-03 17:36:20 -0500

    poo

---
+++ JAVA/src/Movies/Actors.java
@@ -0,0 +1,13 @@
+package Movies;
+import java.util.ArrayList;
+
+public class Actors {
+	ArrayList<String> act = new ArrayList<String>();
+	
+	void setActor(String actName) {
+		if (act.contains(actName)) {
+			return;
+		}
+		act.add(actName);
+	}
+}
---
+++ JAVA/src/Movies/Movies.java
@@ -0,0 +1,21 @@
+package Movies;
+import javax.swing.JOptionPane;
+
+public class Movies {
+	
+	public static void main(String[] args) {
+		
+		Dictionary movies = new Dictionary();
+		movies.cargarLista();
+		String window = JOptionPane.showInputDialog("Option 1 or Option 2? (a/b)");
+		if (window.equalsIgnoreCase("a")) {
+			String movie = JOptionPane.showInputDialog("Write the name of two movies separated by |,&,^: " );
+			movies.Dictionaries(movie);
+		}
+		else if(window.equalsIgnoreCase("b")) {
+			String actor = JOptionPane.showInputDialog("Write the name of an actor: ");
+			movies.optionB(actor);
+		}
+		
+	}
+}
